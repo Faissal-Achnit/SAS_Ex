dfa7222bc55575cd82cc06911295792865fbdb45
Achnit Faissal <faissalachnit696@gmail.com>
2026-09-19 11:26:33 +0100

    modifidaction

--- Day1/Ex5.js
+++ Day1/Ex5.js
@@ -1,14 +1,14 @@
-let str = "bonjour";
+let str = "bOnjeUr";
 let count = 0;
 
 for (let i = 0; i < str.length; i++) {
-  if (str[i] === "a" ||
-    str[i] === "e" ||
-    str[i] === "i" ||
-    str[i] === "o" ||
-    str[i] === "u")
-     {
-    count++
+  if (str[i] === "a" ||str[i] === "e" ||str[i] === "i" ||str[i] === "o" || str[i] === "u") 
+    {
+    count++;
+  }
+  else if(str[i] === "A" ||str[i] === "E" ||str[i] === "I" ||str[i] === "O" || str[i] === "U")
+    {
+     count++;
   }
   
 }
